7384e6082967959a1191e0c0319f9ea3bb7db7f2
Ritesh Kumar <quebeqk@gmail.com>
2023-05-09 18:07:57 +0530

    first_commit-01

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+    "h1",
+    {id:"heading"},
+    "Hello World From React!"
+);
+const r=ReactDOM.createRoot(document.getElementById("abc"));
+r.render(heading);
