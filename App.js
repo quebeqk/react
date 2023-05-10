9551bb8e5d95d33037c7ac0ffe556e4a2dfe420f
Ritesh Kumar <quebeqk@gmail.com>
2023-05-10 17:27:33 +0530

    first-commit-01

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
-const heading = React.createElement(
-    "h1",
-    {id:"heading"},
-    "Hello World From React!"
+import React from "react";
+import ReactDOM from "react-dom/client";
+const title =(
+    <h1 className="head" tabIndex="5">
+        React using JSX🛰️
+    </h1>
 );
-const r=ReactDOM.createRoot(document.getElementById("abc"));
-r.render(heading);
+const Title =() =>(
+    <h1 className="head" tabIndex="5">
+        React using JSX🛰️
+    </h1>
+);
+
+const HeadingComponent =() => (
+    <div id="container">
+        <Title />
+        <h1 className="heading">React functional Componet</h1>
+    </div>
+);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+const root1=ReactDOM.createRoot(document.getElementById("root1"));
+
+root.render(<HeadingComponent />);
+root1.render(title)
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# react 
+# React 🛰️
+
+...
+# Parcel
+-Dev Build
+-Local Server
+-HMR = Hot Module Replacement
+-File Watching Algorithm - written in C++
+-Cashing - farster Builds
+-image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistent hashing
+-Code Splitting
+-Differential Bundling - support older browsers
+-Diagnostics
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+Different dev and prod bundles
